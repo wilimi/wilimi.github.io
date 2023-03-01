7c78d7a6ae9a91b295e822c306a30280e7945fa9
wilimi <126523737+wilimi@users.noreply.github.com>
2023-03-01 15:46:58 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022carv",
+      "defaultValue": "2022miesc",
       "required": "true",
       "disabled": "true"
     },
