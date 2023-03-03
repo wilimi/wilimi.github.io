2573903e0017fa119e590fac931b92fafd3526b3
wilimi <126523737+wilimi@users.noreply.github.com>
2023-03-03 08:19:38 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022miesc",
+      "defaultValue": "2023miesc",
       "required": "true",
       "disabled": "true"
     },
