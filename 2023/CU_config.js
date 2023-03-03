e88129e3f6af35379f86c8c690990d472257df09
wilimi <126523737+wilimi@users.noreply.github.com>
2023-03-03 09:27:38 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -55,7 +55,7 @@ var config_data = `
       "min": 1,
       "max": 99999
     },
-    { "name": "Auto Start Position (take a screenshot of this to the desktop in a folder with the match number and label it the team number!)",
+    { "name": "Auto Start Position (take a screenshot of this to the desktop in a folder with the match number and label it the team number or just a Google Doc with results!)",
       "code": "as",
       "type": "clickable_image",
       "filename": "2023/field_image.png",
